45355961acee5dab3e9308f851cd510b278a49b4
EricSSartorius <EricSSartorius@gmail.com>
2019-05-26 16:23:11 -0700

    rename components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Layout from '../components/Layout'
-import SEO from "../components/SEO"
+import Layout from '../components/layout'
+import SEO from "../components/seo"
 
 const Blog = ({ location }) => (
   <Layout location={location}>
